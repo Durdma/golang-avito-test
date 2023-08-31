2c8aa6be280cc1d406ccdaa1ead01fd814d7ec88
Durdma <glodiolusmmm@gmail.com>
2023-08-31 23:52:54 +0300

    Update dbServer.go

--- server/dbServer.go
+++ server/dbServer.go
@@ -8,10 +8,9 @@ import (
 )
 
 func InitDatabase() *gorm.DB {
-	// "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	dbURI := "postgres://postgres:zxc13243546@localhost:5432/User_slug_db"
-	//dsn := "host=localhost user=postgres password=zxc13243546 dbname=User_slug_db port=5432 sslmode=disable TimeZone='Europe/Moscow parseTime=True"
-	// dsn := "host=localhost user=postgres password=zxc13243546 dbname=User_slug_db port=5432 sslmode=disable TimeZone='Europe/Moscow"
+	
+	dbURI := "postgres://postgres:postgres@localhost:5432/User_slug_db"
+	
 	dbHandler, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error while initializing Database %v", err)
