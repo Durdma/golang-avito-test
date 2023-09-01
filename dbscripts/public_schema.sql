695d7bcc3c2df5e135daaea62347ecff9c8efb5f
Durdma <glodiolusmmm@gmail.com>
2023-09-02 00:36:34 +0300

    Не был обновлен файл

--- dbscripts/public_schema.sql
+++ dbscripts/public_schema.sql
@@ -1,33 +1,41 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET client_min_messages = warning;
-SET row_security = off;
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA pg_catalog;
-SET search_path = public, pg_catalog;
-SET default_tablespace = '';
---users
 CREATE TABLE users (
 	user_id int UNIQUE NOT NULL,
+	created_at varchar(150) NOT NULL,
+	updated_at varchar(150) NOT NULL,
 	
 	PRIMARY KEY (user_id)
 );
---slugs
+
 CREATE TABLE slugs (
 	slug_id serial NOT NULL,
 	slug_name varchar(150) UNIQUE NOT NULL,
+	created_at varchar(150) NOT NULL,
+	updated_at varchar(150) NOT NULL,
+	deleted_at varchar(150),
+	disabled int NOT NULL,
 	
 	PRIMARY KEY (slug_id)
 );
---users_slugs
+
 CREATE TABLE users_slugs (
-	fk_user_id integer NOT NULL,
-	fk_slug_id integer NOT NULL,
+	user_user_id integer NOT NULL,
+	slug_slug_id integer NOT NULL,
+	created_at varchar(150),
+	updated_at varchar(150),
+	deleted_at varchar(150),
+	until_date varchar(150),
 	
-	PRIMARY KEY(fk_user_id, fk_slug_id),
-	FOREIGN KEY(fk_user_id) REFERENCES users(user_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-	FOREIGN KEY(fk_slug_id) REFERENCES slugs(slug_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	PRIMARY KEY(user_user_id, slug_slug_id),
+	FOREIGN KEY (user_user_id) REFERENCES users(user_id),
+	FOREIGN KEY (slug_slug_id) REFERENCES slugs(slug_id)
+);
+
+CREATE TABLE histories (
+	operation_id serial NOT NULL,
+	user_user_id integer NOT NULL,
+	slug_name varchar(150) NOT NULL,
+	operation varchar(50) NOT NULL,
+	date_info varchar(150) NOT NULL,
+	
+	PRIMARY KEY (operation_id)
 );
